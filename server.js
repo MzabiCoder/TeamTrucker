67187f710902e3e3119fa55adbd6a32e5189d5a8
Nabil Fannane <nabil@goadroit.com>
2020-08-21 19:59:32 -0700

    last commit and prepare for deploy

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express()
 const UsersRoutes=require('./api/routes/users')
 const ContactsRoutes=require('./api/routes/contacts')
 const AuthRoutes = require('./api/routes/auth')
+const path=require('path')
 
 const DBconnection = require('./config/DB')
 
@@ -14,6 +15,15 @@ app.use('/api/users',UsersRoutes)
 app.use('/api/contacts',ContactsRoutes)
 app.use('/api/auth',AuthRoutes)
 
+
+// Server static asset in production
+if (process.env.NODE_ENV === 'production') {
+     // Set static folder
+     app.use(express.static('client/build'))
+     app.get('*', (req, res) => {
+          res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+     })
+}
 
 const PORT=process.env.PORT || 5000
 
